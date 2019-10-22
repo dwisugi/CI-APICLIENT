a97f4630920b153821735dd96a188aaa71111777
dwisugi <dwisugianto17@gmial.com>
2019-10-22 21:10:13 +0700

    mulai

---
+++ application/modelssssss/Mahasiswa_model.php
@@ -0,0 +1,134 @@
+<?php 
+use GuzzleHttp\Client;
+
+use function GuzzleHttp\json_decode;
+
+class Mahasiswa_model extends CI_model {
+
+    private $_client; 
+
+    public function __construct()
+    {
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost/RestApiCiEdit/api/',
+            'auth' => ['admin', '1234'],
+            'WPU-KEY' => 'wpu123'
+        ]);
+    }
+
+    public function getAllMahasiswa()
+    {
+        // $client = new Client();
+
+        $response = $this->_client->request('GET', 'mahasiswa', [
+            // 'query' => [
+            //     'WPU-KEY' => 'wpu123'
+            // ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result['data'];
+        // return $this->db->get('mahasiswa')->result_array();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        // $client = new Client();
+
+        $response = $this->_client->request('GET', 'mahasiswa', [
+            'query' => [
+                // 'WPU-KEY' => 'wpu123',
+                'id' => $id
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result['data'][0];
+        // return $this->db->get_where('mahasiswaas', ['id' => $id])->row_array();
+    }
+
+    public function tambahDataMahasiswa() 
+    {
+        $response = $this->_client->request('POST', 'mahasiswa', [
+            'form_params' => [
+            "nama" => $this->input->post('nama'),
+            "nrp" => $this->input->post('nrp'),
+            "email" => $this->input->post('email'),
+            "jurusan" => $this->input->post('jurusan')
+            // 'WPU-KEY' => 'wpu123'
+                // 'id' => $id
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result;
+
+        // $data = [
+        //     "nama" => $this->input->post('nama', true),
+        //     "nrp" => $this->input->post('nrp', true),
+        //     "email" => $this->input->post('email', true),
+        //     "jurusan" => $this->input->post('jurusan', true)
+        // ];
+
+        // $this->db->insert('mahasiswa', $data);
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $response = $this->_client->request('DELETE', 'mahasiswa', [
+            'form_params' => [
+                // 'WPU-KEY' => 'wpu123',
+                'id' => $id
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result;
+        // $this->db->where('id', $id);
+        // $this->db->delete('mahasiswa', ['id' => $id]);
+    }
+ 
+
+
+    public function ubahDataMahasiswa()
+    {
+        $response = $this->_client->request('PUT', 'mahasiswa', [
+            'form_params' => [
+            "id"    => $this->input->post('id'),
+            "nama" => $this->input->post('nama'),
+            "nrp" => $this->input->post('nrp'),
+            "email" => $this->input->post('email'),
+            "jurusan" => $this->input->post('jurusan')
+            // 'WPU-KEY' => 'wpu123'
+                // 'id' => $id
+            ]
+        ]);
+
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result;
+        // $data = [
+        //     "nama" => $this->input->post('nama', true),
+        //     "nrp" => $this->input->post('nrp', true),
+        //     "email" => $this->input->post('email', true),
+        //     "jurusan" => $this->input->post('jurusan', true)
+        // ];
+
+        // $this->db->where('id', $this->input->post('id'));
+        // $this->db->update('mahasiswa', $data);
+    }
+
+    public function cariDataMahasiswa()
+    {
+        $keyword = $this->input->post('keyword', true);
+        $this->db->like('nama', $keyword);
+        $this->db->or_like('jurusan', $keyword);
+        $this->db->or_like('nrp', $keyword);
+        $this->db->or_like('email', $keyword);
+        return $this->db->get('mahasiswa')->result_array();
+    }
+}
---
+++ application/modules/mahasiswa/controllers/Mahasiswa.php
@@ -0,0 +1,101 @@
+<?php
+
+class Mahasiswa extends MX_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_m');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['judul'] = 'Daftar Mahasiswa'; 
+        $data['mahasiswa'] = $this->Mahasiswa_m->getAllMahasiswa();
+        if( $this->input->post('keyword') ) {
+            $data['mahasiswa'] = $this->Mahasiswa_m->cariDataMahasiswa();
+        }
+        // $data['admin'] = $this->Mahasiswa_m->admin(); 
+        $this->load->view('v_templates/header', $data);
+        $this->load->view('v_mahasiswa/index', $data);
+        $this->load->view('v_templates/footer'); 
+    }
+
+    public function login()
+    {
+        $data['judul'] = 'Login';
+
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        // $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('v_templates/header', $data);
+            $this->load->view('v_mahasiswa/adminya');
+            $this->load->view('v_templates/footer');
+        } else {
+            // $this->Mahasiswa_m->admin();
+            $data['admin'] = $this->Mahasiswa_m->admin();
+            $this->session->set_flashdata('flash', $data['admin']);
+            redirect('mahasiswa');
+        }
+    }
+
+    public function tambah()
+    {
+        $data['judul'] = 'Form Tambah Data Mahasiswa';
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('v_templates/header', $data);
+            $this->load->view('v_mahasiswa/tambah');
+            $this->load->view('v_templates/footer');
+        } else {
+            $this->Mahasiswa_m->tambahDataMahasiswa();
+            $this->session->set_flashdata('flash', 'Ditambahkan');
+            redirect('mahasiswa');
+        }
+    }
+
+    public function hapus($id)
+    {
+        $this->Mahasiswa_m->hapusDataMahasiswa($id);
+        $this->session->set_flashdata('flash', 'Dihapus');
+        redirect('mahasiswa');
+    }
+
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Data Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_m->getMahasiswaById($id);
+        $this->load->view('v_templates/header', $data);
+        $this->load->view('v_mahasiswa/detail', $data);
+        $this->load->view('v_templates/footer');
+    } 
+
+    public function ubah($id)
+    {
+        $data['judul'] = 'Form Ubah Data Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_m->getMahasiswaById($id);
+        $data['jurusan'] = ['Teknik Informatika', 'Teknik Mesin', 'Teknik Planologi', 'Teknik Pangan', 'Teknik Lingkungan'];
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('v_templates/header', $data);
+            $this->load->view('v_mahasiswa/ubah', $data);
+            $this->load->view('v_templates/footer');
+        } else {
+            $this->Mahasiswa_m->ubahDataMahasiswa();
+            $this->session->set_flashdata('flash', 'Diubah');
+            redirect('mahasiswa');
+        }
+    }
+
+}
